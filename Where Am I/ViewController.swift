bff36ba087d466777bd9d288f5377aa20050c7c1
Luiz Fernando Santiago <luizfso@gmail.com>
2015-10-15 23:24:41 -0300

    Learning to use GeoLocation and get some info from there. Nice App!

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -7,12 +7,77 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var manager: CLLocationManager!
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations)
+        
+        
+        
+        var userLocation:CLLocation = locations[0]
+        
+        self.latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        
+        self.longitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        
+        self.courseLabel.text = "\(userLocation.course)"
+        
+        self.speedLabel.text = "\(userLocation.speed)"
+        
+        self.altitudeLabel.text = "\(userLocation.altitude)"
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) -> Void in
+            
+            if (error != nil){
+                
+                print(error)
+            
+            } else {
+            
+                if let p = placemarks?[0]{
+                    
+                    var subThoroughfare:String = ""
+                    
+                    if (p.subThoroughfare != nil){
+                        
+                        subThoroughfare = p.subThoroughfare!
+                    
+                    }
+                    
+                    self.addressLabel.text = "\(subThoroughfare) \(p.thoroughfare) \n \(p.subLocality) \n \(p.subAdministrativeArea) \n \(p.postalCode) \n \(p.country)"
+                
+                }
+            
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
